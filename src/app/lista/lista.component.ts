da9111872028a800f4676fe91c93a9b4fa908715
lguzmandiaz <leonardo.guzmandiaz@altran.com>
2020-06-09 10:39:23 +0200

    add behavior to show the structural and attributes directives

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -7,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
+  isVisible = true;
+  identifier = 'listLeo';
+  list = [
+    {id: '1', name: 'leo', active: true},
+    {id: '2', name: 'maria', active: false},
+    {id: '3', name: 'marcos', active: true}
+  ];
+
+  private obj = {
+    name: 'leo',
+    surname: 'guzman',
+    id: 1,
+  };
+
   constructor() {
   }
 
   ngOnInit(): void {
+
+    for (let i = 0; i < 10; i++) {
+      console.log(i);
+    }
+
+    for (const item of this.list) {
+      console.log(item);
+    }
+
+    for (const item in this.obj) {
+      console.log(item);
+    }
   }
 
+  changeVisibility() {
+    this.isVisible = !this.isVisible;
+  }
+
+  changeStatus(item) {
+    item.active = !item.active;
+  }
 }
